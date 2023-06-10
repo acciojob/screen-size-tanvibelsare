7dc48952deecd74425aee301efb1a8a348584745
tanvibelsare <118069064+tanvibelsare@users.noreply.github.com>
2023-06-10 13:37:14 +0530

    created a submission

--- script.js
+++ script.js
@@ -1,1 +1,9 @@
 //your JS code here. If required.
+function getSize(){
+let size=document.getElementById("sizeInfo");
+let x=window.innerWidth;
+let y=window.innerHeight;
+size.innerHTML=`
+<h1>Width:${x}   and Height: ${y}  </h1>
+	`
+}
