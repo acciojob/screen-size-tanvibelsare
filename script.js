34c5057ed04315b52b7c33e37646fe0e04c8fca4
tanvibelsare <118069064+tanvibelsare@users.noreply.github.com>
2023-06-10 19:35:26 +0530

    created a submission

--- script.js
+++ script.js
@@ -3,7 +3,7 @@ function getSize(){
 let size=document.getElementById("sizeInfo");
 let x=window.innerWidth;
 let y=window.innerHeight;
-size.innerHTML=`
-<h1>Width:${x}   and Height: ${y}  </h1>
-	`
+size.innerHTML=
+"<h1>Width:"+x+" and Height:"+ y +"</h1>"
+	
 }
